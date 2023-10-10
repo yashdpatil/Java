220ebe6252be8ed48e45268e3de23cbb7ded18a5
Yash Patil <102399703+yashdpatil@users.noreply.github.com>
2023-10-10 21:18:19 +0530

    Delete PW directory

--- PW/check_arraysortd.java
+++
@@ -1,8 +0,0 @@
-public class check_arraysortd {
-
-    public static void main(String[] args) {
-        int n = 2;
-        System.out.println((n / 2) + 1);
-
-    }
-}
--- PW/exam.py
+++
@@ -1,7 +0,0 @@
-# t = int(int())
-t=int(input())
-p = int(input())
-q = int(input())
-c= p/2
-print(int(c))
-
