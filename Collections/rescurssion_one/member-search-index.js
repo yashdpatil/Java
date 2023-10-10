10341b149523fa8810869c5426b3b5339b9c00cb
Yash Patil <102399703+yashdpatil@users.noreply.github.com>
2023-10-10 21:16:44 +0530

    Delete Collections directory

--- Collections/rescurssion_one/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"rescurssion_one","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"rescurssion_one","l":"rescurssion_one()","u":"%3Cinit%3E()"}];updateSearchResults();
