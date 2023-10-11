cc4f63365742df6a0423e547fe11a12339f84d48
Yash Patil <102399703+yashdpatil@users.noreply.github.com>
2023-10-11 09:25:29 +0530

    Update Basic_copyies.java

--- Cdac_java/Basic_copyies.java
+++ Cdac_java/Basic_copyies.java
@@ -1,1 +1,26 @@
-
+class A implements Cloneable{
+    int a =20;
+    int b =30;
+    int c= 30;
+    public A clone(){
+        try{
+        A aaa = (A)super.clone();
+        return aaa;
+        }catch(Exception e){
+            throw new InternalError(e);
+        }
+        
+    }
+    
+}
+public class Main{
+    public static void main(String[] args){
+        A aa = new A();
+        // Shallow Copy
+        A b =aa; 
+        // Deep Copy
+        A b = aa.clone();
+        
+        System.out.println(aa==b);
+    }
+}
