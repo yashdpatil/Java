bfaee203d9fdecf7a4d72816c877c70f0ba16b75
Yash Patil <102399703+yashdpatil@users.noreply.github.com>
2023-10-16 00:03:16 +0530

    Update String_vowel.java

--- String_vowel.java
+++ String_vowel.java
@@ -1,1 +1,25 @@
+/*
+Create a Java program that does the following: 
+1. Accepts a sentence input from the user. 
+2. Splits the sentence into words without using any collection data structures. 
+3. Removes all vowels from each word. 
+4. Reconstruct and print the sentence without vowels. 
+*/
 
+import java.util.*;
+
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String Pattern = "[AEIOUaeiou]";
+        String[] st = str.split("//s+");
+        StringBuffer sb = new StringBuffer();
+        for(String strr : st){
+            String modstr = strr.replaceAll(Pattern,"");
+            sb.append(modstr).append(" ");
+            
+        }
+        System.out.println(sb.toString());
+    }
+}
