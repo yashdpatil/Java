665b8af1e013c7c261c34f3aa3fecffa7c567ac4
Yash Patil <102399703+yashdpatil@users.noreply.github.com>
2023-11-05 21:29:47 +0530

    Update StringBuilder_reverse.java

--- StringBuilder_reverse.java
+++ StringBuilder_reverse.java
@@ -1,1 +1,18 @@
-
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+		System.out.println("Hello World");
+		
+		String s = sc.next();
+		StringBuilder st = new StringBuilder(s);
+		StringBuilder p =new StringBuilder(s).reverse();
+		if(st.toString().equals(p.toString())){
+		    System.out.println("Pallindrome");
+		    
+		}else{
+		    System.out.println("Not a palindrome");
+		}
+	}
+}
